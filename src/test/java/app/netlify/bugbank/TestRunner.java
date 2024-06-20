788f6401bc01e7b3c85c51093f504bb7ee5a0891
loopfagundes <lof.fagundes@gmail.com>
2024-06-20 15:25:38 -0300

    adicionei extends abstract

--- src/test/java/app/netlify/bugbank/TestRunner.java
+++ src/test/java/app/netlify/bugbank/TestRunner.java
@@ -1,4 +1,24 @@
 package app.netlify.bugbank;
 
-public class TestRunner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        plugin = {
+                "summary",
+                "html:reports/report.html",
+                "json:reports/report.json",
+        },
+        glue = { "app.netlify.bugbank.stepdefs" },
+        features = "src/test/resources/features"
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = false)
+    public Object[][] scenarios() {
+        return super.scenarios();
+
+    }
 }
